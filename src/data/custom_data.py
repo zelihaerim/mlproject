8f7acb7262e20cb210a82b3c1c9eb44ea3e39be7
zelihaerim <zerim@gtu.edu.tr>
2024-11-05 22:37:34 +0300

    zerim : custom data and model trainer files are added.

--- src/data/custom_data.py
+++ src/data/custom_data.py
@@ -1,32 +1,9 @@
 import sys
 import pandas as pd
 from src.exception import CustomException
-from src.utils import load_object
-
-
-class PredictPipeline:
-    def __init__(self):
-        pass
-
-    def predict(self,features):
-        try:
-            model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            print("Before Loading")
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
-            return preds
-        
-        except Exception as e:
-            raise CustomException(e,sys)
-
-
 
 class CustomData:
-    def __init__(  self,
+    def __init__(self,
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
@@ -65,4 +42,3 @@ class CustomData:
 
         except Exception as e:
             raise CustomException(e, sys)
-
